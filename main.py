11bb7ea9ef032453bc8754fc1e382e0cd7bb2701
frankib200 <135721404+frankib200@users.noreply.github.com>
2023-06-06 18:40:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 from tensorflow import keras
 import tensorflow as tf
 tf.config.set_visible_devices([], 'GPU')
